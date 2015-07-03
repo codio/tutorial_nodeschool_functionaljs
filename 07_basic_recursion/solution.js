f259cc351e4c3afbcff0f9ff45f3ad76fde8f291
starbuck451 <robstarbuck@gmail.com>
2015-07-03 13:28:14 +0100

    Update solution.js
    Moved 'arr.length' into a variable called 'arrlen' outside into the closure in order that it need not be calculated every time the function is run.

--- 07_basic_recursion/solution.js
+++ 07_basic_recursion/solution.js
@@ -1,6 +1,7 @@
 function reduce(arr, fn, initial) {
+  var arrlen = arr.length;
   return (function reduceOne(index, value) {
-    if (index > arr.length - 1) return value
+    if (index > arrlen - 1) return value
     return reduceOne(index + 1, fn(value, arr[index], index, arr))
   })(0, initial)
 }
